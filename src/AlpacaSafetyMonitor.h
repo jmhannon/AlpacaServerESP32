c5af37e807563dbabd34b6222b9c6c94b3f38fb9
agnunez <anunez@gmail.com>
2023-11-25 18:50:16 +0100

    Bug uppercase in _device_type

--- src/AlpacaSafetyMonitor.h
+++ src/AlpacaSafetyMonitor.h
@@ -9,7 +9,7 @@ class AlpacaSafetyMonitor : public AlpacaDevice
         // alpaca commands
         void aGetInterfaceVersion(AsyncWebServerRequest *request);
         virtual void aGetIsSafe(AsyncWebServerRequest *request) = 0;
-        AlpacaSafetyMonitor() {strcpy(_device_type, "SafetyMonitor");}
+        AlpacaSafetyMonitor() {strcpy(_device_type, "safetymonitor");}
     public:
         void registerCallbacks();
 };
