0477f4ffb9225384a2c0c365aa5e6f454cbd3fb1
jmhannon <jimmnannon@gmail.com>
2024-07-30 12:57:04 -0500

    Added cover calibarator corrected error codes in AlpacaHelpers

--- src/AlpacaHelpers.h
+++ src/AlpacaHelpers.h
@@ -10,21 +10,22 @@
 // discovery package
 typedef struct
 {
-    private:
-        char _header[15];
-        char _version;
-        char _reserved[48];
-    public:
-        bool valid() { return strncmp(_header, ALPACA_DISCOVERY_HEADER, 15) == 0; }
-        char version() { return _version; }
+private:
+    char _header[15];
+    char _version;
+    char _reserved[48];
+
+public:
+    bool valid() { return strncmp(_header, ALPACA_DISCOVERY_HEADER, 15) == 0; }
+    char version() { return _version; }
 } AlpacaDiscoveryPacket;
 static_assert(sizeof(AlpacaDiscoveryPacket) == ALPACA_DISCOVERY_LENGTH, "Wrong size of struct");
 
-typedef union {
+typedef union
+{
     AlpacaDiscoveryPacket data;
     char buffer[ALPACA_DISCOVERY_LENGTH];
 } AlpacaDiscoveryBuffer;
-
 
 // return
 #define ALPACA_API_VERSIONS "[1]"
@@ -41,19 +42,19 @@ typedef union {
 #define ALPACA_RESPOSE_VALUE_ERROR "{\n\t\"Value\": %s,\n\t\"ClientTransactionID\": %i,\n\t\"ServerTransactionID\": %i,\n\t\"ErrorNumber\": %i,\n\t\"ErrorMessage\": \"%s\"\n}"
 #define ALPACA_RESPOSE_VALUE_ERROR_STR "{\n\t\"Value\": \"%s\",\n\t\"ClientTransactionID\": %i,\n\t\"ServerTransactionID\": %i,\n\t\"ErrorNumber\": %i,\n\t\"ErrorMessage\": \"%s\"\n}"
 
-
-enum AscomErrorCode : int64_t {
-    ActionNotImplementedException = 0x8004040C, // to indicate that the requested action is not implemented in this driver.
-    DriverBase = 0x80040500,                    // The starting value for driver-specific error numbers.
-    DriverMax = 0x80040FFF,                     // The maximum value for driver-specific error numbers.
-    InvalidOperationException = 0x8004040B,     // to indicate that the requested operation can not be undertaken at this time.
-    InvalidValue = 0x80040401,                  // for reporting an invalid value.
-    InvalidWhileParked = 0x80040408,            // used to indicate that the attempted operation is invalid because the mount is currently in a Parked state.
-    InvalidWhileSlaved = 0x80040409,            // used to indicate that the attempted operation is invalid because the mount is currently in a Slaved state.
-    NotConnected = 0x80040407,                  // used to indicate that the communications channel is not connected.
-    NotImplemented = 0x80040400,                // for property or method not implemented.
-    NotInCacheException = 0x8004040D,           // to indicate that the requested item is not present in the ASCOM cache.
-    SettingsProviderError = 0x8004040A,         // related to settings.
-    UnspecifiedError = 0x800404FF,              // used when nothing else was specified.
-    ValueNotSet = 0x80040402                    // for reporting that a value has not been set.
+enum AscomErrorCode : int64_t
+{
+    ActionNotImplementedException = 0x40C, // to indicate that the requested action is not implemented in this driver.
+    DriverBase = 0x500,                    // The starting value for driver-specific error numbers.
+    DriverMax = 0xFFF,                     // The maximum value for driver-specific error numbers.
+    InvalidOperationException = 0x40B,     // to indicate that the requested operation can not be undertaken at this time.
+    InvalidValue = 0x401,                  // for reporting an invalid value.
+    InvalidWhileParked = 0x408,            // used to indicate that the attempted operation is invalid because the mount is currently in a Parked state.
+    InvalidWhileSlaved = 0x409,            // used to indicate that the attempted operation is invalid because the mount is currently in a Slaved state.
+    NotConnected = 0x407,                  // used to indicate that the communications channel is not connected.
+    NotImplemented = 0x400,                // for property or method not implemented.
+    NotInCacheException = 0x40D,           // to indicate that the requested item is not present in the ASCOM cache.
+    SettingsProviderError = 0x40A,         // related to settings.
+    UnspecifiedError = 0x4FF,              // used when nothing else was specified.
+    ValueNotSet = 0x402                    // for reporting that a value has not been set.
 };
